f9b356945ca8722bd9fbdee691bc83a73844a419
Fuflik1752 <sas.chikunova@mail.ru>
2024-06-09 16:41:48 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+a = ("Я_твой_друг")
+print(len(a))
+
+first = 5
+second = 4
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+first = 5
+second = 4
+third = 7
+mean = (first + second + third) / 3
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник,'
+print(second_string, first_string)
+
+a = 5
+b = 4
+c = 7
+f = (((a * b) + (a * c))**3) / 2
+print(f)
